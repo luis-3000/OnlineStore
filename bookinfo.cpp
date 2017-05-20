3aa4a231103255d95885498e0cf22c32cbbf4c83
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-19 23:34:29 -0500

    Setting up this file to connect it to other parts of the porject by changing the main() function name to bookInfo(). Other changes will follow.

--- bookinfo.cpp
+++ bookinfo.cpp
@@ -7,18 +7,18 @@
 #include "iostream"
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+void bookInfo () {
 
-   cout << "\n\neBookStore Booksellers\n";
-   cout << "\t\t\tBook Information\n\n";
+   cout << "\n\t\teBookStore Booksellers\n";
+   cout << "\t\t   Book Information\n\n";
    cout << "ISBN:\n";
    cout << "Title\n";
    cout << "Author:\n";
    cout << "Publisher:\n";
    cout << "Date Added:\n";
    cout << "Quantity-On-Hand:\n";
-   cout << "Wholesale Cost:\n"
-   cout << "Retail Price:\n"
+   cout << "Wholesale Cost:\n";
+   cout << "Retail Price:\n";
 
    return 0;
 }

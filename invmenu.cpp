3d259b2c7f2ec5ff12cd307c8d0e178babb345a3
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-21 16:23:28 -0500

    Modified the 'switch' statement in the invmenu.cpp file to call the corresponding functions based on users' selection of actions

--- invmenu.cpp
+++ invmenu.cpp
@@ -28,38 +28,14 @@ void deleteBook() {
    cout << "Deleting the book from your cart ... " << endl;
 }
 
+
 void invmenu() {
+//int main() {
 //int inventory(int choice) {
    int choice;
-   cout << "\neBookStore Booksellers\n";
-   cout << "\t\tInventory Database\n\n";
-   cout << "1. Look Up a Book\n";
-   cout << "2. Add a Book\n";
-   cout << "3. Edit a Book's Record\n";
-   cout << "4. Delete a Book\n";
-   cout << "5. Return to the Main Menu\n\n";
-   cout << "Enter Your Choice: ";
-   cin >> choice;
-   
-   //Take appropriate actions
-   switch(choice)
-   {
-      case 1: cout << "You selected item  1.\n";
-      break;
-      case 2: cout << "You selected item  2.\n";
-      break;
-      case 3: cout << "You selected item  3.\n";
-      break;
-      case 4: cout << "You selected item  4.\n";
-      break;
-      case 5: cout << "You selected item  5.\n";
-      break;
-      default: cout << "\nPlease enter a number in the range 1-4.\n\n";
-      break;
-   }
 
    do {
-   		cout << "eBookStore Booksellers\n";
+   		cout << "\n\teBookStore Booksellers\n";
 		   cout << "\t\t\tInventory Database\n\n";
 		   cout << "1. Look Up a Book\n";
 		   cout << "2. Add a Book\n";
@@ -71,21 +47,21 @@ void invmenu() {
          //Take appropriate actions
          switch(choice)
          {
-            case 1: cout << "You selected item  1.\n";
+            case 1: lookUpBook();
             break;
-            case 2: cout << "You selected item  2.\n";
+            case 2: addBook();
             break;
-            case 3: cout << "You selected item  3.\n";
+            case 3: editBook();
             break;
-            case 4: cout << "You selected item  4.\n";
+            case 4: deleteBook();
             break;
-            case 5: cout << "You selected item  5.\n";
+            case 5: cout << "Returnning to the main menu ...\n";
             break;
             default: cout << "\nPlease enter a number in the range 1-4.\n\n";
             break;
          }
    } while (choice != 5);
 
-
+   return 0;
 }
 

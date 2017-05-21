c5e5390006ad785fcc10a0b9c219b7fcd5e61313
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-21 16:54:09 -0500

    Modified the 'Switch' statement in the reports.cpp file

--- reports.cpp
+++ reports.cpp
@@ -21,7 +21,7 @@ void repRetail() {
 }
 
 void repQty() {
-   cou << "You selected Listing By Quantity.";
+   cout << "You selected Listing By Quantity.";
 }
 
 void repCost() {
@@ -32,40 +32,10 @@ void repAge() {
    cout << "You selected Listing By Age.";
 }
 
+//int main() {
 void reports() {
-   int choice;
-   cout << "\n\teBookStore Booksellers\n";
-   cout << "\t\tReports\n\n";
-   cout << "1. Inventory Listing\n";
-   cout << "2. Inventory Wholesale Value\n";
-   cout << "3. Inventory Retail Value\n";
-   cout << "4. Listing by Quantity\n";
-   cout << "5. Listing by Cost\n";
-   cout << "6. Listing by Age\n";
-   cout << "7. Return to the Main Menu\n\n";
-   cout << "Enter Your Choice: ";
-   cin >> choice;
    
-   //Take appropriate actions
-   switch(choice)
-   {
-      case 1: cout << "You selected item  1.\n";
-      break;
-      case 2: cout << "You selected item  2.\n";
-      break;
-      case 3: cout << "You selected item  3.\n";
-      break;
-      case 4: cout << "You selected item  4.\n";
-      break;
-      case 5: cout << "You selected item  5.\n";
-      break;
-      case 6: cout << "You selected item  5.\n";
-      break;
-      case 7: cout << "You selected item  5.\n";
-      break;
-      default: cout << "\nPlease enter a number in the range 1-4.\n\n";
-      break;
-   }
+   int choice;
 
    do {
          cout << "\n\teBookStore Booksellers\n";
@@ -83,23 +53,23 @@ void reports() {
          //Take appropriate actions
          switch(choice)
          {
-            case 1: cout << "You selected item  1.\n";
+            case 1: repListing();
             break;
-            case 2: cout << "You selected item  2.\n";
+            case 2: repWholesale();
             break;
-            case 3: cout << "You selected item  3.\n";
+            case 3: repRetail();
             break;
-            case 4: cout << "You selected item  4.\n";
+            case 4: repQty();
             break;
-            case 5: cout << "You selected item  5.\n";
+            case 5: repCost();
             break;
-            case 6: cout << "You selected item  6.\n";
+            case 6: repAge();
             break;
-            case 7: cout << "You selected item  7.\n";
+            case 7: cout << "Returning to the main menu ...\n";
             break;
-            default: cout << "\nPlease enter a number in the range 1-4.\n\n";
+            default: cout << "\nPlease enter a number in the range 1-7.\n\n";
             break;
          }
    } while (choice != 7);
-
+   //return 0;
 }

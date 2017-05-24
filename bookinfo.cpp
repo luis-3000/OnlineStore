03a92083950cc544806f866e1d5baa7adaa4ee2e
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-23 19:28:15 -0500

    Added parameters to the function to display pertinent data.Will complete functionality next

--- bookinfo.cpp
+++ bookinfo.cpp
@@ -7,11 +7,12 @@
 #include <iostream>
 using namespace std;
 
-void bookInfo () {
+void bookInfo (char[14] _isbn, char[51] _bookTitle, char[31] _author, char[31] _publisher, 
+                               char[11] date, int _qty, double _wholesale, double _retail) {
 
    cout << "\n\t\teBookStore Booksellers\n";
    cout << "\t\t   Book Information\n\n";
-   cout << "ISBN:\n";
+   cout << "ISBN: " <<  << "\n";
    cout << "Title\n";
    cout << "Author:\n";
    cout << "Publisher:\n";
@@ -20,4 +21,8 @@ void bookInfo () {
    cout << "Wholesale Cost:\n";
    cout << "Retail Price:\n";
 
+}
+void display(char[] itemToDisplay) {
+   //Iterate through the char array and print out each character
+   
 }

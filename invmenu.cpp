f2867b55e25631981bd899a049f3f52cdf7e799f
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-24 21:39:44 -0500

    Added code to request book info from the user

--- invmenu.cpp
+++ invmenu.cpp
@@ -21,35 +21,76 @@ using namespace std;
    double wholesale[20];
    double retail[20];
 
+void copyStringIntoArray(string sourceString, char destArray[], int size) {
+   //Iterate through the char array and print out each character
+   for(int i = 0; i < size; i++) {
+      destArray[i] = sourceString.charAt(i);
+   }
+}   
+
 // Stub functions
 void lookUpBook() {
    cout << "Looking up your book now ... " << endl;
 }
 
-void addBook(string _title, string _author, string _publisher, string _dateAdded,
-                     int _qtyOnHand, double _wholesage, double _retail) {
+void addBook() {
+   string _title;
+   string _author;
+   string _publisher;
+   string _dateAdded;
+   int _qtyOnHand;
+   double _wholesage;
+   double _retail;
+
    //cout << "Adding the book you selected to your cart ... " << endl;
    //cout << "The size of bookTitle array is: " 
    //      << sizeof(bookTitle)/sizeof(bookTitle[0]);
-   int size = sizeof(bookTitle)/sizeof(bookTitle[0]);
-   for(int i = 0; i < bookTitle.size; i++) {
-      if(bookTitle[i] == 0 || bookTitle[i] == '\0') { // If empty spot
-         // Add the title at this empty index.
-         bookTitle[i] = _title;
-         // Also add all other pertinent data at the same index for the
-         // other arrays that will hold data for this book
-         isbn[i] = _isbn;
-         author[i] = _author;
-         publisher[i] = _publisher;
-         dateAdded[i] = _dateAdded;
-         qtyOnHand[i] = _qtyOnHand;
-         wholesale[i] = _wholesage;
-         retail[i] = _retail;
-      }
-      if(i == size) { // full array, cannot store Database
+   int size = sizeof(bookTitle)/sizeof(bookTitle[0]); // Calculate bookTitle's size
+   for(int i = 0; i < size; i++) {
+
+      // Check if array is full, cannot store Database
+      if(i == size) { 
          cout << "No more books can be added to the inventory. Sorry!\n";
          return;
       }
+
+      if(bookTitle[i] == 0 || bookTitle[i] == '\0') { // If empty spot
+         // Add the title at this empty index.
+         cout << "Enter the title of the booK: ";
+         cin >> _title;
+         bookTitle[i] = _title;
+         
+         // Also add all other pertinent data at the same index for the
+         // other arrays that will hold data for this book
+         cout << "Enter the ISBN of this book: ";
+         cin >> _isbn;
+         isbn[i] = _isbn;
+         
+         cout << "Enter the author of this book: ";
+         cin >> _author;
+         author[i] = _author;
+
+         cout << "Enter the publisher of this book: ";
+         cin >> _publisher;
+         publisher[i] = _publisher;
+
+         cout << "Date this book is being added: ";
+         cin >> _dateAdded;   
+         dateAdded[i] = _dateAdded;
+
+         cout << "Current quantity on hand by adding this book: ";
+         cin >> _qtyOnHand;
+         qtyOnHand[i] = _qtyOnHand;
+
+         cout << "Enter the wholesale price of this book: ";
+         cin >> _wholesage;
+         wholesale[i] = _wholesage;
+
+         cout << "Enter the retail price of this book: ";
+         cin >> _retail;         
+         retail[i] = _retail;
+      }
+
    }
 }
 
